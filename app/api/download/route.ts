e4db90b16e3cd2ebcc1c3d0a38b946cce42af6bd
joannayounes <joanna.younes93@gmail.com>
2025-08-20 23:24:09 +0400

    Update route.ts

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,3 +1,4 @@
+export const runtime = "nodejs";
 import { NextRequest, NextResponse } from "next/server";
 import { verifySession } from "../../../lib/token";
 import { getPurchases } from "../../../lib/store";
